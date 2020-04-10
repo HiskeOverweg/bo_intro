c2883209b20e89e5bcc9f93f66cd9d15317694e7
Hiske <t-hiover@microsoft.com>
2020-04-10 09:50:31 +0100

    create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='bo_intro',
+      version='0.1',
+      description='simple bayesian optimization example',
+      url='http://github.com/storborg/funniest',
+      author='Hiske Overweg',
+      license='MIT',
+      packages=['bo_intro'],
+      zip_safe=False)
